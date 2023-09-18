8f5efd0c08df50e682bd1d687456d64b6a7abe94
Mohamed Koubaa <koubaa@github.com>
2023-09-18 07:44:32 -0500

    use enum name in repr

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -147,10 +147,7 @@ def test_enum_repr():
     """Test enumeration repr."""
     from System import DayOfWeek
 
-    assert repr(DayOfWeek.Monday) == "<DayOfWeek.Monday: 1>"
-
-    assert repr(Test.FlagsEnum(7)) == "<FlagsEnum.Two, Five: 0x00000007>"
-    assert repr(Test.FlagsEnum(8)) == "<FlagsEnum.8: 0x00000008>"
+    assert repr(DayOfWeek.Monday) == "Monday"
 
 
 def test_enum_conversion():
